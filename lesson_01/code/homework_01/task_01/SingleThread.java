ef30deeff9cc3fddef1b54e7985eb425e5429db2
Dimitri Heinrich <dimi-mail@gmx.de>
2024-07-25 12:11:50 +0200

    lesson_01 && homework

---
+++ lesson_01/code/homework_01/task_01/SingleThread.java
@@ -0,0 +1,20 @@
+package homework_01.task_01;
+public class SingleThread {
+
+    private static int counter;
+
+    public static void main(String[] args) {
+        int result = runSingleThreadTask();
+        System.out.println("Сумма в один поток" + " " + result);
+    }
+
+    public static int runSingleThreadTask() {
+        counter = 0; // Reset counter
+        for (int i = 0; i < 2_000_000; i++) {
+            if (i % 21 == 0 && String.valueOf(i).contains("3")) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ lesson_01/code/multithreading/task_03/MyThread.java
@@ -0,0 +1,12 @@
+package multithreading.task_03;
+
+
+public class MyThread extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1_000_000; i++) {
+            Counter.counterIncrement();
+        }
+    }
+}
